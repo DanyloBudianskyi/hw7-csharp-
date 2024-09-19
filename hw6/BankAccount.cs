fd2fa262365ee0fa6210e196a76a9267ccaf97c3
Danil <super.danil501@gmail.com>
2024-09-18 18:16:51 -0600

    Add project files.

---
+++ hw6/BankAccount.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hw7
+{
+    public class BankAccount
+    {
+        public string AccountNumber { get; set; }
+        public decimal Balance { get; set; }
+        public BankAccount(string accountNumber, decimal balance) {
+            AccountNumber = accountNumber;
+            Balance = balance;
+        }
+        public delegate void BankOperationHandler(object sender, BankOperationEventArgs e);
+        public event BankOperationHandler? OnBankOperation;
+
+        public void Deposit(decimal amount)
+        {
+            Balance += amount;
+            OnBankOperation.Invoke(this, new BankOperationEventArgs(this, amount, "Deposit"));
+        }
+        public void Withdraw(decimal amount)
+        {
+            if (Balance >= amount)
+            {
+                Balance -= amount;
+                OnBankOperation.Invoke(this, new BankOperationEventArgs(this, amount, "Withdraw"));
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"Number: {AccountNumber}, Balance: {Balance}";
+        }
+    }
+
+    public class TransactionManager
+    {
+        public BankAccount _BankAccount;
+
+        public TransactionManager(BankAccount bankAccount)
+        {
+            _BankAccount = bankAccount;
+        }
+        public void Deposit(decimal amount)
+        {
+            _BankAccount.Deposit(amount);
+        }
+        public void Withdraw(decimal amount)
+        {
+            _BankAccount.Withdraw(amount);
+        }
+    }
+
+    public class BankOperationEventArgs
+    {
+        public BankAccount Account;
+        public decimal Amount { get; }
+        public string OperationType { get; }
+
+        public BankOperationEventArgs(BankAccount account, decimal amount, string operationType)
+        {
+            Account = account;
+            Amount = amount;
+            OperationType = operationType;
+        }
+    }
+}
---
+++ hw6/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace hw7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            BankAccount account1 = new BankAccount("123456", 1000m);
+            BankAccount account2 = new BankAccount("789012", 2000m);
+            BankAccount account3 = new BankAccount("345678", 500m);
+
+            account1.OnBankOperation += (sender, e) => {
+                Console.WriteLine($"Operation: {e.OperationType}, Account: {e.Account.AccountNumber}, New Balance: {e.Account.Balance}");
+            };
+            account2.OnBankOperation += (sender, e) => {
+                Console.WriteLine($"Operation: {e.OperationType}, Account: {e.Account.AccountNumber}, New Balance: {e.Account.Balance}");
+            };
+            account3.OnBankOperation += (sender, e) => {
+                Console.WriteLine($"Operation: {e.OperationType}, Account: {e.Account.AccountNumber}, New Balance: {e.Account.Balance}");
+            };
+
+            TransactionManager manager1 = new TransactionManager(account1);
+            TransactionManager manager2 = new TransactionManager(account2);
+            TransactionManager manager3 = new TransactionManager(account3);
+
+            manager1.Withdraw(200m);
+            manager2.Withdraw(500m);
+            manager3.Deposit(100m);   
+        }
+    }
+}
